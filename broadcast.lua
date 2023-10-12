c082c201e40613b8f5fa7d4a0cf5f50dd7dbbbfe
Aleksander Spro <abspro@gmail.com>
2023-10-12 12:25:55 +0200

    Add receiver to interface

--- broadcast.lua
+++ broadcast.lua
@@ -39,7 +39,6 @@ BroadCast.usetimestamp = false
 BroadCast.broadcastLevel = 'info'
 BroadCast.prefix = ''
 BroadCast.separator = '::'
-BroadCast.reciever = ''
 
 local broadCastInterface = broadCastInterfaceFactory()
 
@@ -62,9 +61,10 @@ local function GetAbbreviation(bci, level)
 end
 
 ---@param paramLogLevel BroadCastLevels
+---@param recievers string|string[]
 ---@param message string
 ---@param ... string
-local function Output(paramLogLevel, message, ...)
+local function Output(paramLogLevel, recievers, message, ...)
   if not broadCastInterface then
     print("Not been able to load <BroadCastInterface>. Requires <DanNet> or <EQBC> connection.")
     return
@@ -72,41 +72,46 @@ local function Output(paramLogLevel, message, ...)
 
   local broadcastLevel = broadcastLevels[paramLogLevel]
   if broadcastLevels[BroadCast.broadcastLevel:lower()].level <= broadcastLevel.level then
-    local recievers = split(BroadCast.reciever, ",")
+    recievers = (type(recievers) == 'string') and {recievers} or recievers
     local logMessage = string.format(message, ...)
     broadCastInterface.Broadcast(string.format('%s%s %s %s', BroadCast.prefix, GetAbbreviation(broadCastInterface, broadcastLevel), BroadCast.separator, logMessage), recievers)
     mq.delay(BroadCast.delay)
   end
 end
 
+---@param recievers string|string[]
 ---@param message string
 ---@param ... string
-function BroadCast.Info(message, ...)
-  Output('info', message, ...)
+function BroadCast.Info(recievers, message, ...)
+  Output('info', recievers, message, ...)
 end
 
+---@param recievers string|string[]
 ---@param message string
 ---@param ... string
-function BroadCast.Success(message, ...)
-  Output('success', message, ...)
+function BroadCast.Success(recievers, message, ...)
+  Output('success', recievers, message, ...)
 end
 
+---@param recievers string|string[]
 ---@param message string
 ---@param ... string
-function BroadCast.Fail(message, ...)
-  Output('fail', message, ...)
+function BroadCast.Fail(recievers, message, ...)
+  Output('fail', recievers, message, ...)
 end
 
+---@param recievers string|string[]
 ---@param message string
 ---@param ... string
-function BroadCast.Warn(message, ...)
-  Output('warn', message, ...)
+function BroadCast.Warn(recievers, message, ...)
+  Output('warn', recievers, message, ...)
 end
 
+---@param recievers string|string[]
 ---@param message string
 ---@param ... string
-function BroadCast.Error(message, ...)
-  Output('error', message, ...)
+function BroadCast.Error(recievers, message, ...)
+  Output('error', recievers, message, ...)
 end
 
 return BroadCast
