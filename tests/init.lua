e3bbea06454f84d2a17357bd3761991e780a0222
Aleksander Spro <abspro@gmail.com>
2023-10-12 14:56:31 +0200

    Beware of eqbc channel join, will overwrite

--- tests/init.lua
+++ tests/init.lua
@@ -13,7 +13,7 @@ end
 
 if not mq.TLO.Plugin("mq2dannet").IsLoaded() and mq.TLO.Plugin("mq2eqbc").IsLoaded() and mq.TLO.EQBC.Connected() then
   print("EQBC tests")
-  mq.cmd("/bcga //bccmd channels tests")
+  -- mq.cmd("/bcga //bccmd channels tests")
   interfaceTests()
   mq.delay(1000)
   broadcastTests()
